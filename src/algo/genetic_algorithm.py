0610d18738557e7581193359c9d0aa1c435ff25e
Yutai Zhou <yutai.zhou@ll.mit.edu>
2022-05-23 12:29:34 -0400

    added tournament and roulette wheel selection

--- src/algo/genetic_algorithm.py
+++ src/algo/genetic_algorithm.py
@@ -1,4 +1,5 @@
 import logging
+from select import select
 import numpy as np
 from typing import Union, Tuple
 
@@ -40,6 +41,7 @@ class GeneticAlgorithm(NEAlgorithm):
     def __init__(self,
                  param_size: int,
                  selection: str = "truncation",
+                 elitesm: bool = True,
                  pop_size: int = 64,
                  elite_size: int = None,
                  sigma: float = 0.01,
@@ -72,14 +74,43 @@ class GeneticAlgorithm(NEAlgorithm):
         def truncation(fitness: ndarray, params: ndarray) -> ndarray:
             params = params[fitness.argsort(axis=0)] # params sorted from least to most fit
             best_param = params[-1]
-            params = params[-self.elite_size:] # params truncated to most fit K individuals
-            # sample pop_size individuals from elite population
+
+            params = params[-self.elite_size:]
             params = jax.random.choice(self.rng_key, params, shape=[self.pop_size-1])
             params = jnp.vstack((params, best_param))
             return params
+        
+        def tournament(fitness: ndarray, params: ndarray) -> ndarray:
+            params = params[fitness.argsort(axis=0)] # params sorted from least to most fit
+            best_param = params[-1]
+
+            winner_idxes = jax.random.choice(
+                self.rng_key,
+                self.pop_size,
+                shape=[self.pop_size-1, self.elite_size]
+                ).max(axis=1)
+            params = params[winner_idxes]
+            params = jnp.vstack((params, best_param))
+            return params
+        
+        def roulette(fitness: ndarray, params: ndarray) -> ndarray:
+            inds = fitness.argsort(axis=0)
+            params = params[inds] # params sorted from least to most fit
+            best_param = params[-1]
+
+            p = jax.nn.softmax(fitness[inds])
+            params = jax.random.choice(self.rng_key, params, shape=[self.pop_size-1], p = p)
+            params = jnp.vstack((params, best_param))
+            return params
+
 
         self.ask_fn = jax.jit(gaussian_mutation)
-        self.tell_fn = jax.jit(truncation)
+        if selection == "truncation":
+            self.tell_fn = jax.jit(truncation)
+        elif selection == "tournament":
+            self.tell_fn = jax.jit(tournament)
+        elif selection == "roulette":
+            self.tell_fn = jax.jit(roulette)
 
     def ask(self) -> jnp.ndarray:
         self.rng_key, self.params = self.ask_fn(self.rng_key, self.params)
